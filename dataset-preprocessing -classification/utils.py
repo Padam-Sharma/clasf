a605939a0a1a36cefad9fc741e0c02d9fa6bdffd
Padam Sharma <padam@attentive.ai>
2023-05-24 16:03:54 +0530

    a

--- dataset-preprocessing -classification/utils.py
+++ dataset-preprocessing -classification/utils.py
@@ -5,6 +5,9 @@ import random
 import numpy as np
 from tqdm.notebook import tqdm
 import os
+import torch
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
 
 
 def make_dir(path):
@@ -14,6 +17,7 @@ def make_dir(path):
 def rmdir(path):
     shutil.rmtree(path)
 
+
 def movettv(path, type):
     for i in tqdm(path):
         cls = i.split('/')[-2]
@@ -22,3 +26,36 @@ def movettv(path, type):
         dst = '/content/' + type + '/' + cls + '/' + img
         shutil.copy(src, dst)
 
+
+Resize_Transformation = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize(size=(512, 512)),
+])
+Horizontal_Flipping_Transformation = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.RandomHorizontalFlip(p=1)
+])
+Vertical_Flipping_Transformation = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.RandomVerticalFlip(p=1)
+])
+Color_Transformation = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.ColorJitter(brightness=(0.1, 0.6), contrast=1, saturation=0, hue=0.4)
+])
+Contrast_Transformation = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.RandomAutocontrast(p=1)
+])
+Rotate_Transformation = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.RandomRotation(degrees=66)
+])
+
+
+def get_len_aug(type):
+    print(len(glob('/content/train_aug/' + type + '/*')))
+
+
+def get_len(type):
+    print(len(glob('/content/train/' + type + '/*')))
