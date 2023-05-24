4a56f5e78e05569d20b3682ef114566e6a713383
Padam Sharma <padam@attentive.ai>
2023-05-24 14:43:42 +0530

    a

--- dataset-preprocessing -classification/move_images_to_single_folder.py
+++ dataset-preprocessing -classification/move_images_to_single_folder.py
@@ -9,18 +9,20 @@ import os
 
 
 def move_to_single_folder_main(config, logger):
-    img_pth = config["PATH"]["drive"]["image_path"]
-    csv_pth = config["PATH"]["drive"]["image_path"]
-    pwd_root_pth = config["PATH"]["pwd"]["root_path"]
+    img_pth = config['PATH']['drive']['image_path']
+    csv_pth = config['PATH']['drive']['csv_1_path']
+    pwd_root_pth = config['PATH']['pwd']['root_path']
+    org_img_combined_path = config["PATH"]['pwd']['org_img_combined_path']
+    combined_img_path = config["PATH"]['pwd']['combined_img_path']
+
     move_csv_cmd = f"cp {csv_pth} {pwd_root_pth}"
     move_img_cmd = f"cp -r {img_pth} {pwd_root_pth}"
     os.system(move_csv_cmd)
     os.system(move_img_cmd)
-    df = pd.read_csv('/content/corrected data.csv')
 
-    print(len(set(list(df['oid']))), len(list(df['oid'])))
+    df = pd.read_csv(csv_pth)
 
-    (list(df['Duplicate'])).count(2)
+    print('No. of unique rows :', len(set(list(df['oid']))), 'No. of total rows :', len(list(df['oid'])))
 
     make_dir('all_images')
     make_dir('images')
@@ -28,20 +30,23 @@ def move_to_single_folder_main(config, logger):
     make_dir('images/hoa')
     make_dir('images/res')
 
-    test_pth = glob('/content/property classf data/test/*/*')
-    train_pth = glob('/content/property classf data/train/*/*')
-    val_pth = glob('/content/property classf data/val/*/*')
+    test_pth = glob(img_pth+'/test/*/*')
+    train_pth = glob(img_pth+'/train/*/*')
+    val_pth = glob(img_pth+'/val/*/*')
 
     print(len(test_pth) + len(train_pth) + len(val_pth))
 
+    print('Moving images from train, val and test to single folder')
     all_pth = test_pth + train_pth + val_pth
     for i in tqdm(all_pth):
         src = i
-        dst = '/content/all_images/' + i.split('/')[-1]
+        dst = org_img_combined_path + i.split('/')[-1]
         shutil.copy(src, dst)
+    print('Moved...')
 
-    print(len(glob('/content/all_images/*')))
+    print('No. of images after moving :',len(glob(org_img_combined_path+'/*')))
 
+    print('Starting to split images by class...')
     duplicates = []
     cls_map = {'Comm': 'com', 'HOA': 'hoa', 'Resi': 'res'}
     for i in df.iterrows():
@@ -50,10 +55,11 @@ def move_to_single_folder_main(config, logger):
         cls = i[1]['correct_class']
         if oid not in duplicates:
             duplicates.append(oid)
-            src = '/content/all_images/' + oid + '.png'
-            dst = '/content/images/' + cls_map[cls] + '/' + oid + '.png'
+            src = org_img_combined_path + oid + '.png'
+            dst = combined_img_path + cls_map[cls] + '/' + oid + '.png'
             shutil.copy(src, dst)
         else:
             pass
+    print('Split complete...')
 
-    print(len(glob('/content/images/*/*')))
+    print('No. of images after split', len(glob(combined_img_path+'/*/*')))
