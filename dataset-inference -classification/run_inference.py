1eb07dd1eda0d5ac90b04740b9e92f58b4ba0713
Padam Sharma <padam@attentive.ai>
2023-05-25 16:05:54 +0530

    Update run_inference.py

--- dataset-inference -classification/run_inference.py
+++ dataset-inference -classification/run_inference.py
@@ -6,14 +6,14 @@ def run_inference_main(config, logger):
     os.system('pip install -q -e "/content/pytorch-image-models"')
     os.system('pip install -q focal_loss_torch')
 
-    test_py_pth = config['test']['test_py_pth']
-    test_dir = config['test']['test_dir']
-    model_type = config['test']['model_type']
-    saved_model_path = config['test']['saved_model_path']
-    batch_size = config['test']['batch_size']
-    img_size = config['test']['img_size']
-    num_classes = config['test']['num_classes']
-    log_freq = config['test']['log_freq']
+    test_py_pth = config['inference']['test_py_pth']
+    test_dir = config['inference']['test_dir']
+    model_type = config['inference']['model_type']
+    saved_model_path = config['inference']['saved_model_path']
+    batch_size = config['inference']['batch_size']
+    img_size = config['inference']['img_size']
+    num_classes = config['inference']['num_classes']
+    log_freq = config['inference']['log_freq']
 
     test_cmd = f'python {test_py_pth} --data-dir {test_dir} --model {model_type} --checkpoint {saved_model_path} --img-size {img_size} --batch-size {batch_size} --num-classes {num_classes} --log-freq {log_freq}'
 
